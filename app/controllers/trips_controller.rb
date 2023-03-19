241733eb9088df7cd09057383a9b4a44c82b5f39
Eewei Seah <seaheewei@gmail.com>
2023-03-19 16:06:33 +0800

    sorted events by day

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show]
 
   def show
-    @events = @trip.events
+    @days = @trip.events.order(:start_time).group_by { |event| event.start_time.day }.values
   end
 
   private
@@ -10,4 +10,6 @@ class TripsController < ApplicationController
   def set_trip
     @trip = Trip.find(params[:id])
   end
+
+
 end
