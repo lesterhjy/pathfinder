ea9be8cbc32f96c00a001bff52635a5dbb5ba724
Damien Koh <damienkwk@hotmail.sg>
2023-03-20 16:45:46 +0800

    autocomplete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :trips, only: [:show] do
+  resources :trips, only: %i[show create] do
     resources :events, only: [:new, :create]
   end
   resources :recommendations
