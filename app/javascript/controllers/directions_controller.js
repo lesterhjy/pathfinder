f07b9da1f369ce27b6c611f2debd03fa0455e6a8
Eewei Seah <seaheewei@gmail.com>
2023-03-22 13:20:39 +0800

    made sure that transport directions are laid out in the same order

--- app/javascript/controllers/directions_controller.js
+++ app/javascript/controllers/directions_controller.js
@@ -2,26 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="directions"
 export default class extends Controller {
-  static targets = ["directions", "event"]
+  static targets = ["directions", "event", "driving", "walking", "transit"]
 
   connect() {
   }
 
   showDirections() {
 
-    if (event.type === "update-directions" && this.directionsTarget.innerText == "") { return }
+    if (event.type === "update-directions" && this.directionsTarget.innerText === "") { return }
 
     // if there is already stuff loaded in the directions div i.e. google has already been called then hide it
-    if (this.directionsTarget.innerText !== "" && event.type === "click") {
-      this.directionsTargets.forEach((target) => {
-        target.innerHTML = ""
-      })
-    } else {
-      if (event.type === "update-directions") {
-        this.directionsTargets.forEach((target) => {
-          target.innerHTML = ""
-        })
-      }
+    if (this.directionsTarget.innerText !== "" && event.type === "click") { this.wipe() }
+
+    else {
+      if (event.type === "update-directions") { this.wipe() }
+
       this.eventTargets.forEach((eventTarget, index) => {
 
         // console.log(eventTarget.dataset)
@@ -51,20 +46,17 @@ export default class extends Controller {
 
               if (mode === 'DRIVING') {
                 icon = '<i class="fa-solid fa-car-side"></i>'
+                this.drivingTargets[index].innerHTML = `
+                <p class="direction"><small>${icon} ${duration}</small></p>`
               } else if (mode === 'TRANSIT') {
                 icon = '<i class="fa-solid fa-train-subway"></i>'
+                this.transitTargets[index].innerHTML = `
+                <p class="direction"><small>${icon} ${duration}</small></p>`
               } else if (mode === 'WALKING') {
                 icon = '<i class="fa-solid fa-person-hiking"></i>'
+                this.walkingTargets[index].innerHTML = `
+                <p class="direction"><small>${icon} ${duration}</small></p>`
               }
-
-
-              if (mode === "TRANSIT") {
-                mode = "public transport"
-              }
-
-              this.directionsTargets[index].innerHTML += `
-                <p class="direction"><small>${icon} ${duration}</small></p>
-              `
             });
           })
         }
@@ -73,6 +65,18 @@ export default class extends Controller {
 
     }
 
+  }
+
+  wipe() {
+    this.walkingTargets.forEach((target) => {
+      target.innerHTML = ""
+    })
+    this.drivingTargets.forEach((target) => {
+      target.innerHTML = ""
+    })
+    this.transitTargets.forEach((target) => {
+      target.innerHTML = ""
+    })
   }
 
 
