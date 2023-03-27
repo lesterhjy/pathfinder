d6f92e2744fa30dab9401f751564c4a6d08ef35f
Eewei Seah <seaheewei@gmail.com>
2023-03-27 22:23:51 +0800

    made inline-edit a bit nicer

--- app/javascript/controllers/edit_event_controller.js
+++ app/javascript/controllers/edit_event_controller.js
@@ -2,22 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-event"
 export default class extends Controller {
-  static targets = ["card", "form", "info", "sign"]
+  static targets = ["card", "form", "info", "sign", "field"]
 
   connect() {
+    // console.log(this.fieldTargets)
+    // console.log(this.formTargets)
   }
 
   displayForm() {
-    this.formTarget.classList.remove('d-none');
+    let current_event = event.target;
+    let current_index = this.fieldTargets.indexOf(current_event);
+    this.formTargets[current_index].classList.remove('d-none');
+    this.fieldTargets[current_index].classList.add('d-none');
   }
 
   update(event) {
     event.preventDefault()
-    const url = this.formTarget.action
+    let current_event = event.target;
+    let current_index = this.formTargets.indexOf(current_event);
+    const url = this.formTargets[current_index].action
     fetch(url, {
       method: "PATCH",
       headers: {'Accept': 'text/plain'},
-      body: new FormData(this.formTarget)
+      body: new FormData(this.formTargets[current_index])
     })
     .then(response => response.text())
     .then((data) => {
