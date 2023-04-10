827380c1eaa4672fc98a94fdab0292d668ee1a56
Lester Ho <lesterhjy@gmail.com>
2023-04-10 21:38:15 +0800

    adding trip deletion feature

--- app/controllers/user_trips_controller.rb
+++ app/controllers/user_trips_controller.rb
@@ -1,2 +1,10 @@
 class UserTripsController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
+  def destroy
+    @usertrip = UserTrip.find(params[:id])
+    @usertrip.destroy
+    redirect_to trips_path
+  end
 end
